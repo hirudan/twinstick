d715be026cd3f45634061b728bfbf481d22fc297
hirudan <republiqueajamais@hotmail.com>
2021-10-01 01:19:36 -0700

    Implement keyboard movement control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,8 @@ public class PlayerController : MonoBehaviour
     // Parameters that control the turn speed and the amount of ease-in/out time. Adjust to suit taste.
     public float rotationSpeed = 360;
     public float smoothTime = 0.1f;
-    private float _angle, _currentVelocity;
+    public float walkSpeed = 5.0f;
+    private float _angle, _speed, _currentVelocityAngular, _currentVelocityTranslational;
     
     // Start is called before the first frame update
     void Start()
@@ -20,10 +21,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Handle rotation toward camera
+        
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var directionToFace = mousePosition - transform.position;
         var targetAngle = Vector2.SignedAngle(Vector2.up, directionToFace);
-        _angle = Mathf.SmoothDampAngle(_angle, targetAngle, ref _currentVelocity, smoothTime, rotationSpeed);
+        _angle = Mathf.SmoothDampAngle(_angle, targetAngle, ref _currentVelocityAngular, smoothTime, rotationSpeed);
         transform.eulerAngles = new Vector3(0, 0, _angle);
+        
+        // Handle movement
+        _speed = Mathf.SmoothDamp(_speed, walkSpeed, ref _currentVelocityTranslational, smoothTime, walkSpeed);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * (_speed * Time.deltaTime), Space.World);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * (_speed* Time.deltaTime), Space.World);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * (_speed * Time.deltaTime), Space.World);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * (_speed * Time.deltaTime), Space.World);
+        }
     }
 }
